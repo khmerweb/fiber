b9a1c2cc0ad72126cb0fc45ed5c4d1ddd7d61617
khmerweb <vuthdevelop1@gmail.com>
2025-11-21 21:24:13 +0700

    Make it better

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -2,20 +2,37 @@ package frontend
 
 import (
 	"fiber/db"
+	"fmt"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/session"
+	//"github.com/gofiber/fiber/v2/middleware/session"
 )
 
 func FrontRoutes(router fiber.Router) {
 	router.Get("/", func(c *fiber.Ctx) error {
-		sess := c.Locals("session").(*session.Session)
-		name := sess.Get("name").(string)
-		count := db.CountPosts("movie")
+		//sess := c.Locals("session").(*session.Session)
+		//name := sess.Get("name").(string)
+		var count int64
+		var counts []int64
+		categories := []string{"news", "movie", "travel", "game", "sport", "doc", "food", "music", "distraction"}
+		for _, value := range categories {
+			count = db.CountPosts(value)
+			counts = append(counts, count)
+		}
+		/*
+			jsonDatas, err := json.Marshal(counts)
+			if err != nil {
+				fmt.Println("Error marshalling string data:", err)
+				return err
+			}
 
+			jsonCounts := string(jsonDatas)
+		*/
+		fmt.Println(counts)
 		return c.Render("index", fiber.Map{
-			"Title": "ដំណឹង​ល្អ " + name,
-			"Count": count,
+			"Title":   "ដំណឹង​ល្អ ",
+			"Counts":  counts,
+			"PageURL": "/",
 		}, "base")
 	})
 
