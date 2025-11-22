172bd7eefc7e44ef922250d2a2f80ac163301f88
khmerweb <vuthdevelop1@gmail.com>
2025-11-22 16:14:54 +0700

    Make it better

--- db/post.go
+++ db/post.go
@@ -9,24 +9,29 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func CountPosts(category string) int64 {
+func CountPosts() []int64 {
 	mongoDB := ConnectDB()
 	collection := mongoDB.Database("blog").Collection("Post")
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	filter := bson.M{
-		"categories": bson.M{
-			"$regex":   category,
-			"$options": "i",
-		},
-	}
 	opts := options.Count().SetHint("_id_")
-
-	count, err := collection.CountDocuments(ctx, filter, opts)
-	if err != nil {
-		log.Fatal(err)
-		return 0
+	var counts []int64
+	categories := []string{"news", "movie", "travel", "game", "sport", "doc", "food", "music", "distraction"}
+	for _, category := range categories {
+		filter := bson.M{
+			"categories": bson.M{
+				"$regex":   category,
+				"$options": "i",
+			},
+		}
+		count, err := collection.CountDocuments(ctx, filter, opts)
+		if err != nil {
+			log.Fatal(err)
+			return []int64{}
+		}
+		counts = append(counts, count)
 	}
+
 	defer mongoDB.Disconnect(ctx)
-	return count
+	return counts
 }
--- frontend/frontend.go
+++ frontend/frontend.go
@@ -2,7 +2,6 @@ package frontend
 
 import (
 	"fiber/db"
-	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 	//"github.com/gofiber/fiber/v2/middleware/session"
@@ -12,23 +11,10 @@ func FrontRoutes(router fiber.Router) {
 	router.Get("/", func(c *fiber.Ctx) error {
 		//sess := c.Locals("session").(*session.Session)
 		//name := sess.Get("name").(string)
-		var count int64
-		var counts []int64
-		categories := []string{"news", "movie", "travel", "game", "sport", "doc", "food", "music", "distraction"}
-		for _, value := range categories {
-			count = db.CountPosts(value)
-			counts = append(counts, count)
-		}
-		/*
-			jsonDatas, err := json.Marshal(counts)
-			if err != nil {
-				fmt.Println("Error marshalling string data:", err)
-				return err
-			}
 
-			jsonCounts := string(jsonDatas)
-		*/
-		fmt.Println(counts)
+		counts := db.CountPosts()
+
+		//fmt.Println(counts)
 		return c.Render("index", fiber.Map{
 			"Title":   "ដំណឹង​ល្អ ",
 			"Counts":  counts,
